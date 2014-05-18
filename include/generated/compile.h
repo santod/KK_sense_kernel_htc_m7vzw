7d509c57efb6beba4a4d0d864b5553a85ebdae08
santod <santod040@gmail.com>
2014-05-17 18:46:45 -0700

    Gesture updates and misc fixups

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Thu May 1 02:48:05 PDT 2014"
+#define UTS_VERSION "#3 SMP PREEMPT Sat May 17 15:34:51 PDT 2014"
 #define LINUX_COMPILE_BY "santod"
 #define LINUX_COMPILE_HOST "DevBoX"
 #define LINUX_COMPILER "gcc version 4.7.4 20131207 (prerelease) (Linaro GCC 4.7-2013.12) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.10-NuK3rn3L-KK-m7vzw_S6_v10"
+#define UTS_RELEASE "3.4.10-NuK3rn3L-KK-m7vzw_S6_v13-DC-test"
